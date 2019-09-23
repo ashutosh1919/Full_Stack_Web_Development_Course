8ab986ab3b17f607b472439a8687037ff4f0f4ba
ashutosh1919 <ashutoshhathidara98@gmail.com>
2019-09-23 22:36:54 +0530

    Grunt Part I

--- Course1/Bootstrap4/conFusion/Gruntfile.js
+++ Course1/Bootstrap4/conFusion/Gruntfile.js
@@ -10,7 +10,25 @@ module.exports = function(grunt) {
           "css/styles.css": "css/styles.scss"
         }
       }
+    },
+    watch: {
+      files: "css/*.scss",
+      tasks: ["sass"]
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ["css/*.css", "*.html", "js/*.js"]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
     }
   });
   grunt.registerTask("css", ["sass"]);
+  grunt.registerTask("default", ["browserSync", "watch"]);
 };
