3f6a8458d57e5d02008d7d90b6772174a76602f0
ashutosh1919 <ashutoshhathidara98@gmail.com>
2019-09-23 21:56:24 +0530

    grunt config done

--- Course1/Bootstrap4/conFusion/Gruntfile.js
+++ Course1/Bootstrap4/conFusion/Gruntfile.js
@@ -1,5 +1,16 @@
 "use strict";
 
 module.exports = function(grunt) {
-  grunt.initConfig({});
+  require("time-grunt")(grunt);
+  require("jit-grunt")(grunt);
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          "css/styles.css": "css/styles.scss"
+        }
+      }
+    }
+  });
+  grunt.registerTask("css", ["sass"]);
 };
