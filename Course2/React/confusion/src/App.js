84be1eb92397f0fb4d5875988f55a3dd2f3b7489
ashutosh1919 <ashutoshhathidara98@gmail.com>
2019-10-08 19:49:57 +0530

    Components Part I

--- Course2/React/confusion/src/App.js
+++ Course2/React/confusion/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
