ea5860ca0e65bd674dec2f7f4e090f7655fae996
ashutosh1919 <ashutoshhathidara98@gmail.com>
2019-10-17 18:33:20 +0530

    Header and Footer

--- Course2/React/confusion/src/components/MainComponent.js
+++ Course2/React/confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
 import DishdetailComponent from "./DishdetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -21,11 +22,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu
           dishes={this.state.dishes}
           onClick={dishId => this.onDishSelect(dishId)}
@@ -37,6 +34,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer/>
       </div>
     );
   }
