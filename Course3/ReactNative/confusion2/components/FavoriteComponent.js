6bc1cb628df40125bb914bfe2286d04bde29c55c
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-02-01 16:06:29 +0530

    Animations

--- Course3/ReactNative/confusion2/components/FavoriteComponent.js
+++ Course3/ReactNative/confusion2/components/FavoriteComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, Text, Alert } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -31,7 +31,26 @@ class Favorites extends Component {
         {
           text: "Delete",
           type: "delete",
-          onPress: () => this.props.deleteFavorite(item.id)
+          onPress: () => {
+            Alert.alert(
+              "Delete Favorite ?",
+              "Are you sure you wish to delete the favorite dish " +
+                item.name +
+                " ?",
+              [
+                {
+                  text: "Cancel",
+                  onPress: () => console.log(item.name + "Not Deleted"),
+                  style: "cancel"
+                },
+                {
+                  text: "OK",
+                  onPress: () => this.props.deleteFavorite(item.id)
+                }
+              ],
+              { cancelable: false }
+            );
+          }
         }
       ];
       return (
--- Course3/ReactNative/confusion2/components/HomeComponent.js
+++ Course3/ReactNative/confusion2/components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Text, View, Animated, Easing } from "react-native";
 import { Card } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -42,31 +42,80 @@ function RenderItem(props) {
 }
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0);
+  }
+
   static navigationOptions = {
     title: "Home"
   };
 
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    this.animatedValue.setValue(0);
+    Animated.timing(this.animatedValue, {
+      toValue: 8,
+      duration: 8000,
+      easing: Easing.linear
+    }).start(() => this.animate());
+  }
+
   render() {
+    const xpos1 = this.animatedValue.interpolate({
+      inputRange: [0, 1, 3, 5, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    const xpos2 = this.animatedValue.interpolate({
+      inputRange: [0, 2, 4, 6, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    const xpos3 = this.animatedValue.interpolate({
+      inputRange: [0, 3, 5, 7, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
     return (
-      <ScrollView>
-        <RenderItem
-          item={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
-          isLoading={this.props.dishes.isLoading}
-          errMess={this.props.dishes.errMess}
-        />
-        <RenderItem
-          item={
-            this.props.promotions.promotions.filter(promo => promo.featured)[0]
-          }
-          isLoading={this.props.promotions.isLoading}
-          errMess={this.props.promotions.errMess}
-        />
-        <RenderItem
-          item={this.props.leaders.leaders.filter(leader => leader.featured)[0]}
-          isLoading={this.props.leaders.isLoading}
-          errMess={this.props.leaders.errMess}
-        />
-      </ScrollView>
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos1 }] }}
+        >
+          <RenderItem
+            item={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos2 }] }}
+        >
+          <RenderItem
+            item={
+              this.props.promotions.promotions.filter(
+                promo => promo.featured
+              )[0]
+            }
+            isLoading={this.props.promotions.isLoading}
+            errMess={this.props.promotions.errMess}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos3 }] }}
+        >
+          <RenderItem
+            item={
+              this.props.leaders.leaders.filter(leader => leader.featured)[0]
+            }
+            isLoading={this.props.leaders.isLoading}
+            errMess={this.props.leaders.errMess}
+          />
+        </Animated.View>
+      </View>
     );
   }
 }
